eddb13b508faa376b933318447ff83a3deb2d34f
dankbkkmember-cmyk <dankbkkmember@gmail.com>
2026-06-27 16:04:28 +0700

    Create emailjs-config.js

---
+++ assets/js/emailjs-config.js
@@ -0,0 +1,7 @@
+const EMAILJS_CONFIG = {
+  publicKey:    '-4COuGFwyinLANJ3T',
+  serviceId:    'service_rx8gbt9',
+  templateId:   'template_rpb77hq',
+  managerEmail: 'dankclubbkk@gmail.com',
+};
+window.EMAILJS_CONFIG = EMAILJS_CONFIG;
