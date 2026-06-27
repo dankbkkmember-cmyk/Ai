fa78d1237afca1a3dcad7f665914621619fc46b7
dankbkkmember-cmyk <dankbkkmember@gmail.com>
2026-06-27 16:04:06 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL = 'https://yfvzibhxphonoqadsadu.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_MVnhThq96RHCKwU63NuY0Q__6exyPT8';
+const SITE_URL = 'https://dankbkkmember-cmyk.github.io/Ai';
+window.DANK_SUPABASE_URL      = SUPABASE_URL;
+window.DANK_SUPABASE_ANON_KEY = SUPABASE_ANON_KEY;
+window.DANK_SITE_URL          = SITE_URL;
